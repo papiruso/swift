1436d684eb739088ab1a3e53156a598a5fd551ea
nihao <nihaonu@gmail.com>
2020-06-03 22:38:45 +0300

    Initial Commit

---
+++ uncodegem/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uncodegem
+//
+//  Created by dave on 03.06.2020.
+//  Copyright © 2020 uncodegem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
