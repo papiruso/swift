4d843559f90da1831e3a02a9a5060362d7381c91
nihao <nihaonu@gmail.com>
2020-06-05 09:18:07 +0300

    init

--- uncodegem/ViewController.swift
+++ uncodegem/ViewController.swift
@@ -9,12 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+ 
+    @IBOutlet weak var dateTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findDayButton(_ sender: UIButton) {
+        print(dateTF.text!)
     }
-
-
 }
 
